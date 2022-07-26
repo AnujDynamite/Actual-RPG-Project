0677d8e75b7082c304997b062e416b6d2636da9d
AnujDynamite <squidin@hotmail.com>
2022-07-26 13:23:33 +1200

    Mana Regen implemented, Mana and Mana Regen Stats added to Progression.

--- Assets/Scripts/Attributes/Mana.cs
+++ Assets/Scripts/Attributes/Mana.cs
@@ -1,37 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using GameDevTV.Utils;
+using RPG.Stats;
 using UnityEngine;
 
 namespace RPG.Attributes
 {
     public class Mana : MonoBehaviour
     {
-        [SerializeField] float maxMana = 200.0f;
+        // At Awake the game may not have fully finished loading the save data
+        // which is needed for BaseStats to be populated to know the level of the character
+        // and therefore what starting stats they should have.
+        // At Start the component should already be ready for use, other componenets could
+        // be calling this one.
+        // LazyValue allows us to wrap a value as a function that gets initial value.
+        LazyValue<float> mana;
 
-        float mana;
+        //float mana;
 
         private void Awake()
         {
-            mana = maxMana;
+            mana = new LazyValue<float>(GetMaxMana);
+            //mana = maxMana;
+        }
+
+        private void Update()
+        {
+            if(mana.value < GetMaxMana())
+            {
+                mana.value += GetRegenRate() * Time.deltaTime;
+                if(mana.value > GetMaxMana())
+                {
+                    mana.value = GetMaxMana();
+                }
+            }
         }
 
         public float GetMana()
         {
-            return mana;
+            return mana.value;
         }
 
         public float GetMaxMana()
         {
-            return maxMana;
+            return GetComponent<BaseStats>().GetStat(Stat.Mana);
+        }
+
+        public float GetRegenRate()
+        {
+            return GetComponent<BaseStats>().GetStat(Stat.ManaRegenRate);
         }
 
         public bool UseMana(float manaToUse)
         {
-            if(manaToUse > mana)
+            if(manaToUse > mana.value)
             {
                 return false;
             }
-            mana -= manaToUse;
+            mana.value -= manaToUse;
             return true;
         }
     }
